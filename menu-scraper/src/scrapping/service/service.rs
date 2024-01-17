6c20edd739335370548c176fa0f30cc2799bbbcf
Jan Sýkora <485598@muni.cz>
2024-01-17 23:47:05 +0100

    feat: meals for today scrapped

--- menu-scraper/src/scrapping/service/service.rs
+++ menu-scraper/src/scrapping/service/service.rs
@@ -3,15 +3,24 @@ pub fn scrap() {
     let html_content = response.unwrap().text().unwrap();
     let document = scraper::Html::parse_document(&html_content);
     let html_selector = scraper::Selector::parse("div.menicka_detail").unwrap();
-    let menicka = document.select(&html_selector);
-    //println!("{}", menicka.count());
-    for menu in menicka {
-        let info = menu.select(&scraper::Selector::parse("div.nazev").unwrap()).next().unwrap().text().collect::<Vec<_>>();
-        println!("{}", info.first().unwrap().to_string());
-            // .and_then(|a| a.value().attr("info"))
-            // .map(str::to_owned);
-        // if let Some(inside) = info {
-        //     return inside;
-        // }
+    let menu_list = document.select(&html_selector);
+    for menu in menu_list {
+        let info = menu
+            .select(&scraper::Selector::parse("div.nazev").unwrap())
+            .next()
+            .unwrap()
+            .text()
+            .collect::<Vec<_>>();
+        let restaurant_name = info.first().unwrap().to_string();
+
+        println!("{}", restaurant_name);
+
+        let meals = menu
+            .select(&scraper::Selector::parse("div.nabidka_1").unwrap())
+            .map(|m| m.inner_html())
+            .collect::<Vec<_>>();
+        for meal in meals {
+            println!("  {meal}");
+        }
     }
 }
