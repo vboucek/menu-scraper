c416b7d3e39010c840dc062ee317e9601e82dfee
Jan Sýkora <485598@muni.cz>
2024-01-30 22:17:42 +0100

    fix: removed useless code

--- menu-scraper/src/scrapping/service/service.rs
+++ menu-scraper/src/scrapping/service/service.rs
@@ -366,61 +366,6 @@ fn get_restaurant_address(html: &Html) -> anyhow::Result<RestaurantAddress> {
     })
 }
 
-pub fn scrap_menus_today() -> Vec<MenuCreate> {
-    let response = reqwest::blocking::get("https://www.menicka.cz/brno.html");
-    let html_content = response.unwrap().text().unwrap();
-    let document = Html::parse_document(&html_content);
-    let html_selector = Selector::parse("div.menicka_detail").unwrap();
-    let menu_list = document.select(&html_selector);
-    let result: Vec<MenuCreate> = Vec::new();
-    for menu in menu_list {
-        let info = menu
-            .select(&Selector::parse("div.nazev").unwrap())
-            .next()
-            .unwrap()
-            .text()
-            .collect::<Vec<_>>();
-        let restaurant_name = info.first().unwrap().to_string();
-
-        let restaurant_link = menu
-            .select(&Selector::parse("a.noborder").unwrap())
-            .next()
-            .unwrap()
-            .value()
-            .attr("href")
-            .unwrap()
-            .to_owned();
-
-        println!("NÁZEV: {}", restaurant_name);
-        println!("LINK: {}", restaurant_link);
-
-        let meals = menu
-            .select(&Selector::parse("div.nabidka_1").unwrap())
-            .map(|m| m.inner_html())
-            .collect::<Vec<_>>();
-        for meal in meals {
-            let (is_soup, name) = parse_meal_name(meal);
-            if is_soup {
-                println!("  POLÉVKA: {name}");
-            } else {
-                println!("  {name}");
-            }
-        }
-    }
-
-    result
-}
-
-fn parse_meal_name(name: String) -> (bool, String) {
-    let is_soup = name.contains("<i>");
-    if is_soup {
-        let no_tags = name.replace("<i>", "").replace("</i>", "");
-        return (true, remove_trailing_tags(no_tags));
-    }
-
-    (false, remove_trailing_tags(name))
-}
-
 fn remove_trailing_tags(str: String) -> String {
     let regex = Regex::new("^(.*?)<").unwrap();
     let m = regex.find(str.as_str());
