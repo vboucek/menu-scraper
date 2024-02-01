5cf30c839f8e6b4fdeaddebebcb66a7e68f7bdf6
AdamBalu <a.baluseskul@gmail.com>
2024-02-01 13:53:07 +0100

    collapsed lunch views

--- menu-scraper/src/app/handlers/group.rs
+++ menu-scraper/src/app/handlers/group.rs
@@ -1,19 +1,29 @@
 use crate::app::errors::{ApiError, HtmxError};
 use crate::app::forms::group_creation::GroupCreationFormData;
-use crate::app::templates::group::{GroupCreationTemplate, GroupDetailsTemplate, GroupsTemplate};
+use crate::app::forms::lunch::CreateLunchFormData;
+use crate::app::templates::group::{
+    GroupCreateLunchFormTemplate, GroupCreateLunchTemplate, GroupCreationTemplate,
+    GroupDetailsTemplate, GroupsTemplate,
+};
 use crate::app::utils::picture::validate_and_save_picture;
 use crate::app::utils::validation::Validation;
 use crate::app::view_models::signed_user::SignedUser;
 use actix_identity::Identity;
 use actix_multipart::form::MultipartForm;
 use actix_session::Session;
-use actix_web::http::uri::PathAndQuery;
 use actix_web::web::Data;
 use actix_web::{web, HttpResponse};
 use askama::Template;
+use chrono::{Local, NaiveDate};
 use db::db::common::{DbCreate, DbReadMany, DbReadOne};
-use db::db::models::{Group, GroupCreate, GroupGetById, GroupGetGroupsByUser};
-use db::db::repositories::{GroupRepository, GroupRepositoryListUsers};
+use db::db::models::{
+    GetGroupUserByIds, GroupCreate, GroupGetById, GroupGetGroupsByUser, LunchCreate, LunchGetMany,
+    VoteGetMany,
+};
+use db::db::repositories::{
+    GroupRepository, GroupRepositoryCheckUser, GroupRepositoryListUsers, LunchRepository,
+    VoteRepository,
+};
 use uuid::Uuid;
 
 pub fn group_config(config: &mut web::ServiceConfig) {
@@ -21,7 +31,11 @@ pub fn group_config(config: &mut web::ServiceConfig) {
         .service(web::resource("/groups").route(web::get().to(group_index)))
         .service(web::resource("/group-create").route(web::get().to(get_group_create_form)))
         .service(web::resource("/group").route(web::post().to(post_group)))
-        .service(web::resource("/group-details/{id}").route(web::get().to(group_details)));
+        .service(web::resource("/group-details/{id}").route(web::get().to(group_details)))
+        .service(web::resource("/group-create-lunch/{id}").route(web::post().to(create_lunch)))
+        .service(
+            web::resource("/group-create-lunch-form/{id}").route(web::post().to(create_lunch_form)),
+        );
 }
 
 async fn group_index(
@@ -88,7 +102,7 @@ async fn post_group(
     };
 
     // Store group
-    let group = group_repo
+    group_repo
         .create(&GroupCreate {
             name: form.group_name.to_owned(),
             description,
@@ -103,45 +117,83 @@ async fn post_group(
         .finish())
 }
 
-// Get group users as user previews
-async fn get_group_users_previews(
-    group: Group,
-    group_repo: Data<GroupRepository>,
-) -> Result<HttpResponse, HtmxError> {
-    let previews = group_repo
-        .list_group_users(&GroupGetById { id: group.id })
-        .await?;
-
-    Ok(HttpResponse::Ok().body(""))
-}
-
 async fn group_details(
     id: web::Path<Uuid>,
     group_repo: Data<GroupRepository>,
+    lunch_repo: Data<LunchRepository>,
     session: Session,
     identity: Identity,
-) -> Result<HttpResponse, HtmxError> {
+) -> Result<HttpResponse, ApiError> {
     let group_id = id.into_inner();
     let user_id = Uuid::parse_str(identity.id()?.as_ref())?;
 
-    // let mut tx = group_repo.pool_handler.pool.begin().await?;
-
     let signed_user = session.get::<SignedUser>("signed_user")?;
-    // let kekw = group_repo
-    //     .check_user_is_member(identity.id()?.as_ref(), group_id)
-    //     .await?;
+    group_repo
+        .check_user_is_member(&GetGroupUserByIds { user_id, group_id })
+        .await
+        .map_err(|_| ApiError::Unauthorized)?;
 
     let group_by_id = GroupGetById { id: group_id };
 
     let group = group_repo.read_one(&group_by_id).await?;
     let members = group_repo.list_group_users(&group_by_id).await?;
 
+    let lunches = lunch_repo
+        .read_many(&LunchGetMany {
+            group_id: Some(group_id),
+            user_id: Some(user_id),
+            from: None,
+            to: None,
+        })
+        .await?;
+
     let template = GroupDetailsTemplate {
         group,
         signed_user,
         group_members: members,
+        group_lunches: lunches,
     };
 
+    let body = template.render()?;
+
+    Ok(HttpResponse::Ok().body(body))
+}
+
+async fn create_lunch(group_id: web::Path<Uuid>) -> Result<HttpResponse, HtmxError> {
+    let template = GroupCreateLunchTemplate {
+        group_id: group_id.into_inner(),
+        min_selection_date: Local::now().date_naive(),
+    };
+    let body = template.render()?;
+    Ok(HttpResponse::Ok().body(body))
+}
+
+async fn create_lunch_form(
+    lunch_repo: Data<LunchRepository>,
+    form: web::Form<CreateLunchFormData>,
+    user: Identity,
+    group_id: web::Path<Uuid>,
+) -> Result<HttpResponse, HtmxError> {
+    let date = form.date;
+    let group_id = group_id.into_inner();
+    let user_id = Uuid::parse_str(user.id()?.as_ref())?;
+
+    lunch_repo.create(&LunchCreate { date, group_id }).await?;
+
+    let group_lunches = lunch_repo
+        .read_many(&LunchGetMany {
+            group_id: Some(group_id),
+            user_id: Some(user_id),
+            from: None,
+            to: None,
+        })
+        .await?;
+
+    let template = GroupCreateLunchFormTemplate {
+        group_id,
+        group_lunches,
+        date,
+    };
     let body = template.render()?;
 
     Ok(HttpResponse::Ok().body(body))
--- menu-scraper/src/app/templates/group.rs
+++ menu-scraper/src/app/templates/group.rs
@@ -1,6 +1,10 @@
 use crate::app::view_models::signed_user::SignedUser;
 use askama::Template;
-use db::db::models::{Group, GroupCreate, GroupPreview, UserPreview};
+use chrono::NaiveDate;
+use db::db::models::{
+    Group, GroupCreate, GroupPreview, LunchWithGroup, MenuWithRestaurantAndVotes, UserPreview,
+};
+use uuid::Uuid;
 
 #[derive(Template)]
 #[template(path = "groups.html")]
@@ -22,4 +26,20 @@ pub struct GroupDetailsTemplate {
     pub signed_user: Option<SignedUser>,
     pub group: Group,
     pub group_members: Vec<UserPreview>,
+    pub group_lunches: Vec<LunchWithGroup>,
+}
+
+#[derive(Template)]
+#[template(path = "create_lunch_box.html")]
+pub struct GroupCreateLunchTemplate {
+    pub group_id: Uuid,
+    pub min_selection_date: NaiveDate,
+}
+
+#[derive(Template)]
+#[template(path = "create_lunch_button.html")]
+pub struct GroupCreateLunchFormTemplate {
+    pub group_id: Uuid,
+    pub group_lunches: Vec<LunchWithGroup>,
+    pub date: NaiveDate,
 }
